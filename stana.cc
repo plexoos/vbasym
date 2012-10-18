9806e6c74e0c5c5780162cb3fc8cb349bc8682da
fazio <fazio>
2012-10-18 21:04:04 +0000

    *** empty log message ***

--- stana.cc
+++ stana.cc
@@ -138,11 +138,12 @@ int analyzeMuDst(UInt_t maxEventsUser, string inMuDstFileListName, bool isMC,
       //// printf("file1: %s\n", file1);
       //// outF = file1;
       //// outF.ReplaceAll(".MuDst.root","");
-      //// fileG = inMuDstFileListName.c_str();
-      //// fileG.ReplaceAll("MuDst", "geant");
+      fileG = inMuDstFileListName.c_str();
+      fileG.ReplaceAll("MuDst", "geant");
    }
 
    printf("Output file: %s\n", outF.Data());
+   printf("geant file: %s\n", fileG.Data());
 
    printf("TRIG ID: L2BW=%d, L2EW=%d   isMC=%d  useJetFinder=%d\n", idL2BWtrg, idL2EWtrg, isMC, useJetFinder );
 
@@ -162,9 +163,7 @@ int analyzeMuDst(UInt_t maxEventsUser, string inMuDstFileListName, bool isMC,
    if (geant) {
       // get geant file
       ////s.f.-test      StIOMaker *ioMaker = new StIOMaker();
-      ////s.f.-test      printf("\n %s \n\n", fileG.Data());
-      ////s.f.-test      StIOMaker *ioMaker = new StIOMaker();
-      ////s.f.-test      printf("\n %s \n\n", inMuDstFileListName.c_str());
+      ////s.f.-test      printf("\n geant file %s \n\n", fileG.Data());
       ////s.f.-test      cout << " test Salvo " << endl;
       ////s.f.-test      printf("\n %s \n\n", "/home/starreco/reco/pp500/pythia6_422/Wplus_enu/perugia320/y2009a/gheisha_on/p09ig/rcf10010_1005_1000evts.geant.root");
       ////s.f.-test      ioMaker->SetFile(fileG.Data());
@@ -173,7 +172,7 @@ int analyzeMuDst(UInt_t maxEventsUser, string inMuDstFileListName, bool isMC,
       ////s.f.-test      ioMaker->SetBranch("*", 0, "1"); //deactivate all branches
       ////s.f.-test      ioMaker->SetBranch("geantBranch", 0, "r"); //activate geant Branch
       ////s.f.-test      ioMaker->SetBranch("minimcBranch", 0, "r"); //activate geant Branch
-      ////s.f.-test      cout << " test Salvo 2" << endl;
+      cout << " test Salvo 2" << endl;
    }
 
    // Now we add Makers to the chain...
@@ -411,7 +410,7 @@ int analyzeMuDst(UInt_t maxEventsUser, string inMuDstFileListName, bool isMC,
    if (useJetFinder == 2)
       stVecBosMaker->setJetTreeBranch("ConeJets12_100", "ConeJets12_100_noEEMC"); //select jet tree braches used
 
-   stVecBosMaker->setMaxDisplayEve(10); // only first N events will get displayed
+   ////stVecBosMaker->setMaxDisplayEve(10); // only first N events will get displayed
    //set energy scale (works for data and MC - be careful!)
    // S.F. - We must be carefull to put correct numbers for run 2011 for both data and MC separately! 
    //stVecBosMaker->setBtowScale(1.0);
