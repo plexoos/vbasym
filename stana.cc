cd59182bfbe098e8d6bcd6b7b713d46a32eee434
fazio <fazio>
2012-11-02 20:28:02 +0000

    *** empty log message ***

--- stana.cc
+++ stana.cc
@@ -165,12 +165,13 @@ int analyzeMuDst(UInt_t maxEventsUser, string inMuDstFileListName, bool isMC,
    if (geant) {
       // get geant file
       StIOMaker *ioMaker = new StIOMaker();
-      //ioMaker->SetFile(fileG.Data());
+      // ioMaker->SetFile(fileG.Data());
       // ioMaker->SetFile("/star/data56/reco/pp500/pythia6_422/Wplus_enu/perugia320/y2009a/gheisha_on/p09ig/rcf10010_1000_1000evts.geant.root");
-      // ioMaker->SetIOMode("r");
-      // ioMaker->SetBranch("*", 0, "1"); //deactivate all branches
-      // ioMaker->SetBranch("geantBranch", 0, "r"); //activate geant Branch
-      // ioMaker->SetBranch("minimcBranch", 0, "r"); //activate geant Branch
+      ioMaker->SetFile("/star/institutions/mit/balewski/2012-Wsimu-setH-noFilt/eve_geant/jbc310_1_1000evts.geant.root");
+      ioMaker->SetIOMode("r");
+      ioMaker->SetBranch("*", 0, "1"); //deactivate all branches
+      ioMaker->SetBranch("geantBranch", 0, "r"); //activate geant Branch
+      ioMaker->SetBranch("minimcBranch", 0, "r"); //activate geant Branch
    }
 
    // Now we add Makers to the chain...
