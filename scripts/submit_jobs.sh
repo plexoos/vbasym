f2c5f8f7671a82873cd44716b2848f8aff11fd92
fazio <fazio>
2013-01-29 18:34:43 +0000

    *** empty log message ***

--- scripts/submit_jobs.sh
+++ scripts/submit_jobs.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 RUNLIST=$1
-CODE_DIR=/star/u/smirnovd/vbasym/
-#OUT_DIR=/star/u/smirnovd/vbasym_results/
-OUT_DIR=/star/data05/scratch/smirnovd/
+CODE_DIR=/star/u/fazio/offline/users/fazio/vbasym
+#CODE_DIR=/star/u/smirnovd/vbasym/
+OUT_DIR=/star/data05/scratch/fazio
+#OUT_DIR=/star/data05/scratch/smirnovd/
 #STAR_VER=SL11d
 STAR_VER=SL12c
-#STANA_OPTIONS="-j"
+STANA_OPTIONS="-j"
 #STANA_OPTIONS="-n100"
-STANA_OPTIONS=""
+#STANA_OPTIONS=""
 
 export $STAR_VER
 
@@ -23,5 +24,5 @@ echo
 for runNumber in `cat $RUNLIST`
 do
    echo "Submitting job for runNumber = " $runNumber
-   star-submit-template -template scripts/run11_job_template.xml -entities outPath=$OUT_DIR,codePath=$CODE_DIR,runNumber=$runNumber,STAR_VER=$STAR_VER,STANA_OPTIONS=$STANA_OPTIONS
+   star-submit-template -template run11_job_template.xml -entities outPath=$OUT_DIR,codePath=$CODE_DIR,runNumber=$runNumber,STAR_VER=$STAR_VER,STANA_OPTIONS=$STANA_OPTIONS
 done
