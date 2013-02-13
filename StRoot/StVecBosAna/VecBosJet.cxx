8cb000ece71dfe03f216d4d002411af30ee7619f
smirnovd <smirnovd>
2013-02-13 15:23:53 +0000

    *** empty log message ***

---
+++ StRoot/StVecBosAna/VecBosJet.cxx
@@ -0,0 +1,60 @@
+
+#include "VecBosJet.h"
+
+#include "VecBosEvent.h"
+
+
+VecBosJet::VecBosJet() : StJet(), mEvent(0), mVertexId(-1), mMinZDistToVertex(-1)
+{
+}
+
+
+VecBosJet::VecBosJet(StJet &stJet) :
+   StJet(stJet.E(), stJet.Px(), stJet.Py(), stJet.Pz(), stJet.nCell, stJet.charge),
+   mEvent(0), mVertexId(-1), mMinZDistToVertex(-1)
+{
+  nTracks   = stJet.nTracks;
+  nBtowers  = stJet.nBtowers;
+  nEtowers  = stJet.nEtowers;
+  tpcEtSum  = stJet.tpcEtSum;
+  btowEtSum = stJet.btowEtSum;
+  etowEtSum = stJet.etowEtSum;
+  zVertex   = stJet.zVertex;
+  jetEt     = Et();
+  jetPt     = Pt();
+  jetEta    = Eta();
+  jetPhi    = Phi();
+}
+
+
+/** */
+void VecBosJet::Process()
+{
+   if (!mEvent) return;
+
+   // The only info about jet vertex is its z position. Use it to match vertex to jet
+   VecBosVertex *closestVertex = 0;
+
+   VecBosVertexPtrSetIter iVertex = mEvent->mVertices.begin();
+   for ( ; iVertex != mEvent->mVertices.end(); ++iVertex)
+   {
+      VecBosVertex &vertex = **iVertex;
+      Double_t deltaZ = fabs(zVertex - vertex.mPosition.Z());
+
+      if (mMinZDistToVertex < 0 || deltaZ < mMinZDistToVertex) {
+         mMinZDistToVertex = deltaZ;
+         closestVertex = *iVertex;
+      }
+   }
+
+   if (mMinZDistToVertex < VecBosEvent::mMaxVertexJetDeltaZ) {
+      SetVertexId(closestVertex->mId);
+   }
+}
+
+
+void VecBosJet::Print(const Option_t* opt) const
+{
+   Info("Print(const Option_t* opt)", "Jet mVertexId=%d, zVertex=%.1f cm, nTracks=%d\n", mVertexId, zVertex, nTracks);
+
+}
---
+++ StRoot/StVecBosAna/VecBosJet.h
@@ -0,0 +1,69 @@
+#ifndef VecBosJet_h
+#define VecBosJet_h
+
+#include <vector>
+#include <set>
+
+#include "TObject.h"
+
+#include "StThreeVectorF.hh"
+#include "StSpinPool/StJets/StJet.h"
+
+
+class VecBosEvent;
+
+
+// Info about vertex
+class VecBosJet : public StJet
+{
+public:
+
+   VecBosEvent  *mEvent;             //!
+   Short_t       mVertexId;          // mId of the mother vertex
+   Float_t       mMinZDistToVertex;  // distance to closest vertex along z
+
+   VecBosJet();
+   VecBosJet(StJet &stMuVertex);
+  
+   void SetVertexId(Short_t vId) { mVertexId = vId; }
+   void Process();
+   void clear();
+   virtual void Print(const Option_t* opt="") const;
+
+   ClassDef(VecBosJet, 1);
+};
+
+
+typedef std::vector<VecBosJet>         VecBosJetVec;
+typedef VecBosJetVec::iterator         VecBosJetVecIter;
+typedef VecBosJetVec::const_iterator   VecBosJetVecConstIter;
+
+
+inline bool operator==(const VecBosJet& lhs, const VecBosJet& rhs) { return (TLorentzVector) lhs == (TLorentzVector) rhs; }
+inline bool operator!=(const VecBosJet& lhs, const VecBosJet& rhs) { return !operator==(lhs,rhs); }
+//inline bool operator< (const VecBosJet& lhs, const VecBosJet& rhs) { return lhs.mTracks.size() < rhs.mTracks.size(); }
+inline bool operator< (const VecBosJet& lhs, const VecBosJet& rhs) { return lhs.E() < rhs.E(); }
+inline bool operator> (const VecBosJet& lhs, const VecBosJet& rhs) { return  operator< (rhs,lhs); }
+inline bool operator<=(const VecBosJet& lhs, const VecBosJet& rhs) { return !operator> (lhs,rhs); }
+inline bool operator>=(const VecBosJet& lhs, const VecBosJet& rhs) { return !operator< (lhs,rhs); }
+
+struct CompareVecBosJet
+{
+   bool operator()(const VecBosJet& lhs, const VecBosJet& rhs) const { return lhs > rhs; }
+};
+
+struct CompareVecBosJetPtr
+{
+   bool operator()(const VecBosJet* lhs, const VecBosJet* rhs) const { return (*lhs) > (*rhs); }
+};
+
+typedef std::set<VecBosJet, CompareVecBosJet>     VecBosJetSet;
+typedef VecBosJetSet::iterator                    VecBosJetSetIter;
+typedef VecBosJetSet::const_iterator              VecBosJetSetConstIter;
+
+typedef std::set<VecBosJet*, CompareVecBosJetPtr> VecBosJetPtrSet;
+typedef VecBosJetPtrSet::iterator                 VecBosJetPtrSetIter;
+typedef VecBosJetPtrSet::const_iterator           VecBosJetPtrSetConstIter;
+
+#endif
+
