9ca070a0f0d3ee9ede768d671d071c4452838bd0
Dmitri Smirnov <d.s@plexoos.com>
2013-08-20 17:14:15 -0400

    Removed old outdated files

--- scripts/run_stana_example.sh
+++
@@ -1,2 +0,0 @@
-./stana -f /star/institutions/mit/balewski/2012-Wsimu-setH-noFilt/eve_mu/jbc310_1_1000evts.MuDst.root -j -m -n 100
-./stana -f /star/institutions/mit/balewski/2012-Wsimu-setH-noFilt/eve_mu/jbc310_1_1000evts.MuDst.root -m -n 100
