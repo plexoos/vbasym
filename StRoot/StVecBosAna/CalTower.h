9f00bdaccec72f3e5252e667ec46a1a9f70b29b9
Dmitri Smirnov <d.s@plexoos.com>
2013-08-22 12:30:45 -0400

    Doxygen comments added

--- StRoot/StVecBosAna/CalTower.h
+++ StRoot/StVecBosAna/CalTower.h
@@ -12,11 +12,11 @@
 class CalTower : public TObject
 {
 public:
-   TVector3 R;       // extrapolated position of primary track
-   TVector3 Rglob;   // extrapolated position of global track
-   int      id;      // BTOW tower id (!= 0), not used for ETOW
-   int      iEta;
-   int      iPhi;    // eta x phi bin using L2 indexing convention
+   TVector3 R;       ///< Extrapolated position of primary track
+   TVector3 Rglob;   ///< Extrapolated position of global track
+   int      id;      ///< BTOW tower id (!= 0), not used for ETOW
+   int      iEta;    ///< Tower eta bin using L2 indexing convention
+   int      iPhi;    ///< Tower phi bin using L2 indexing convention
 
    UShort_t GetEtaBin() { return iEta; }
    UShort_t GetPhiBin() { return iPhi; }
