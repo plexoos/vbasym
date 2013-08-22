4e12344aec189a14621d0793ffbe7ab088b639b3
Dmitri Smirnov <d.s@plexoos.com>
2013-08-22 11:20:13 -0400

    Comply with one class one file rule

--- StRoot/StVecBosAna/St2011pubMcMaker.h
+++ StRoot/StVecBosAna/St2011pubMcMaker.h
@@ -15,6 +15,19 @@ class StVecBosMaker;
  */
 class St2011pubMcMaker : public StMaker
 {
+public:
+
+   St2011pubMcMaker(const char *name = "2011pubMc");
+   virtual       ~St2011pubMcMaker();
+   virtual Int_t  Init();
+   virtual Int_t  Make();
+   void setHList(TObjArray *x) {HList = x;}
+
+   void AttachWalgoMaker(StVecBosMaker *mk) { wMK = mk;}
+
+   virtual Int_t InitRun  (int runumber) {return 0;}; // Overload empty StMaker::InitRun
+   virtual Int_t FinishRun(int runumber) {return 0;}; // Overload empty StMaker::FinishRun
+
 private:
 
    StVecBosMaker *wMK; // W-algo maker with all data
@@ -33,24 +46,6 @@ private:
    TVector3 mElectronP;
    TVector3 mVertex;
 
-public:
-
-   St2011pubMcMaker(const char *name = "2011pubMc");
-   virtual       ~St2011pubMcMaker();
-   virtual Int_t  Init();
-   virtual Int_t  Make();
-   void setHList(TObjArray *x) {HList = x;}
-
-   void AttachWalgoMaker(StVecBosMaker *mk) { wMK = mk;}
-
-   virtual Int_t InitRun  (int runumber) {return 0;}; // Overload empty StMaker::InitRun
-   virtual Int_t FinishRun(int runumber) {return 0;}; // Overload empty StMaker::FinishRun
-
-   // Displayed on session exit, leave it as-is please ...
-   virtual const char *GetCVS() const {
-      static const char cvs[] = "Tag $Name:  $ $Id: St2011pubMcMaker.h,v 1.4 2013/01/07 23:53:26 smirnovd Exp $ built "__DATE__" "__TIME__ ;
-      return cvs;
-   }
 
    ClassDef(St2011pubMcMaker, 0)
 };
