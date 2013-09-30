bdcbabaf247375c9ec0b7f6cb134ce3f6d72021c
Dmitri Smirnov <d.s@plexoos.com>
2013-09-30 15:17:08 -0400

    New configuration constants and global variables

--- webview/config.php
+++ webview/config.php
@@ -3,6 +3,9 @@
 define("VBASYM_RESULTS_DIR", "/usr/local/vbasym_results/");
 define("VBASYM_RESULTS_WWW", "results/");
 
-$RHIC_BEAMS = array("1" => "BLU", "2" => "YEL");
+$RHIC_BEAM_SUFFIXES = array("0" => "", "1" => "BLU", "2" => "YEL");
+$RHIC_BEAM_HUMAN_DESCR = array("0" => "Blue and yellow beams combined", "1" => "Blue beam", "2" => "Yellow beam");
+$VECBOS_TYPE_SUFFIXES = array("+1" => "plus", "-1" => "minus");
+$VECBOS_TYPE_HUMAN_DESCR = array("+1" => "W<sup>+</sup>", "-1" => "W<sup>-</sup>");
 
 ?>
