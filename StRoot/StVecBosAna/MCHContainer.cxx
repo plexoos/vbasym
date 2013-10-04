e019c1df3767e53e7d4f47d3eba4072598ba7fbb
Dmitri Smirnov <d.s@plexoos.com>
2013-10-04 15:32:52 -0400

    Access MC event data via a pointer

--- StRoot/StVecBosAna/MCHContainer.cxx
+++ StRoot/StVecBosAna/MCHContainer.cxx
@@ -5,6 +5,7 @@
 #include "TText.h"
 
 #include "WBosEvent.h"
+#include "WBosMcEvent.h"
 #include "Globals.h"
 
 #include "utils/H2I.h"
@@ -182,79 +183,80 @@ void MCHContainer::BookHists()
 void MCHContainer::Fill(ProtoEvent &ev)
 {
    WBosEvent& event = (WBosEvent&) ev;
+   WBosMcEvent* mcEvent = (WBosMcEvent*) event.mMcEvent;
 
-   ((TH1*) o["hWBosonE"]) ->Fill(event.mWEvent->mP4WBoson.E());
-   ((TH1*) o["hWBosonPz"])->Fill(event.mWEvent->mP4WBoson.Pz());
-   ((TH1*) o["hWBosonPt"])->Fill(event.mWEvent->mP4WBoson.Pt());
-   ((TH1*) o["hWBosonPt_zoomin"])->Fill(event.mWEvent->mP4WBoson.Pt());
+   ((TH1*) o["hWBosonE"]) ->Fill(mcEvent->mP4WBoson.E());
+   ((TH1*) o["hWBosonPz"])->Fill(mcEvent->mP4WBoson.Pz());
+   ((TH1*) o["hWBosonPt"])->Fill(mcEvent->mP4WBoson.Pt());
+   ((TH1*) o["hWBosonPt_zoomin"])->Fill(mcEvent->mP4WBoson.Pt());
 
-   if (event.mWEvent->mP4WBoson.Mag() > 0)
-      ((TH1*) o["hWBosonEta"])->Fill(event.mWEvent->mP4WBoson.Eta());
+   if (mcEvent->mP4WBoson.Mag() > 0)
+      ((TH1*) o["hWBosonEta"])->Fill(mcEvent->mP4WBoson.Eta());
    else
       ((TH1*) o["hWBosonEta"])->Fill(1e5);
 
-   ((TH1*) o["hNeutrinoE"]) ->Fill(event.mWEvent->mP4Neutrino.E());
-   ((TH1*) o["hNeutrinoPx"])->Fill(event.mWEvent->mP4Neutrino.Px());
-   ((TH1*) o["hNeutrinoPy"])->Fill(event.mWEvent->mP4Neutrino.Py());
-   ((TH1*) o["hNeutrinoPz"])->Fill(event.mWEvent->mP4Neutrino.Pz());
-   ((TH1*) o["hNeutrinoPt"])->Fill(event.mWEvent->mP4Neutrino.Pz());
+   ((TH1*) o["hNeutrinoE"]) ->Fill(mcEvent->mP4Neutrino.E());
+   ((TH1*) o["hNeutrinoPx"])->Fill(mcEvent->mP4Neutrino.Px());
+   ((TH1*) o["hNeutrinoPy"])->Fill(mcEvent->mP4Neutrino.Py());
+   ((TH1*) o["hNeutrinoPz"])->Fill(mcEvent->mP4Neutrino.Pz());
+   ((TH1*) o["hNeutrinoPt"])->Fill(mcEvent->mP4Neutrino.Pz());
 
-   ((TH1*) o["hLeptonE"]) ->Fill(event.mWEvent->mP4Lepton.E());
-   ((TH1*) o["hLeptonPx"])->Fill(event.mWEvent->mP4Lepton.Px());
-   ((TH1*) o["hLeptonPy"])->Fill(event.mWEvent->mP4Lepton.Py());
-   ((TH1*) o["hLeptonPz"])->Fill(event.mWEvent->mP4Lepton.Pz());
-   ((TH1*) o["hLeptonPt"])->Fill(event.mWEvent->mP4Lepton.Pt());
+   ((TH1*) o["hLeptonE"]) ->Fill(mcEvent->mP4Lepton.E());
+   ((TH1*) o["hLeptonPx"])->Fill(mcEvent->mP4Lepton.Px());
+   ((TH1*) o["hLeptonPy"])->Fill(mcEvent->mP4Lepton.Py());
+   ((TH1*) o["hLeptonPz"])->Fill(mcEvent->mP4Lepton.Pz());
+   ((TH1*) o["hLeptonPt"])->Fill(mcEvent->mP4Lepton.Pt());
 
-   ((TH1*) o["hRecoVsGenLeptonPhi"])  ->Fill(event.mWEvent->mP4Lepton.Phi(),   event.GetElectronP3().Phi());
-   ((TH1*) o["hRecoVsGenLeptonPt"])   ->Fill(event.mWEvent->mP4Lepton.Pt(),    event.GetElectronP3().Pt());
-   ((TH1*) o["hRecoVsGenLeptonPz"])   ->Fill(event.mWEvent->mP4Lepton.Pz(),    event.GetElectronP3().Pz());
-   ((TH1*) o["hRecoVsGenLeptonEta"])  ->Fill(event.mWEvent->mP4Lepton.Eta(),   event.GetElectronP3().Eta());
+   ((TH1*) o["hRecoVsGenLeptonPhi"])  ->Fill(mcEvent->mP4Lepton.Phi(),   event.GetElectronP3().Phi());
+   ((TH1*) o["hRecoVsGenLeptonPt"])   ->Fill(mcEvent->mP4Lepton.Pt(),    event.GetElectronP3().Pt());
+   ((TH1*) o["hRecoVsGenLeptonPz"])   ->Fill(mcEvent->mP4Lepton.Pz(),    event.GetElectronP3().Pz());
+   ((TH1*) o["hRecoVsGenLeptonEta"])  ->Fill(mcEvent->mP4Lepton.Eta(),   event.GetElectronP3().Eta());
 
-   ((TH1*) o["hRecoVsGenNeutrinoPhi"])->Fill(event.mWEvent->mP4Neutrino.Phi(), event.GetNeutrinoP3().Phi());
-   ((TH1*) o["hRecoVsGenNeutrinoPt"]) ->Fill(event.mWEvent->mP4Neutrino.Pt(),  event.GetNeutrinoP3().Pt());
-   ((TH1*) o["hRecoVsGenNeutrinoPz"]) ->Fill(event.mWEvent->mP4Neutrino.Pz(),  event.GetNeutrinoP3().Pz());
-   ((TH1*) o["hRecoVsGenNeutrinoEta"])->Fill(event.mWEvent->mP4Neutrino.Eta(), event.GetNeutrinoP3().Eta());
+   ((TH1*) o["hRecoVsGenNeutrinoPhi"])->Fill(mcEvent->mP4Neutrino.Phi(), event.GetNeutrinoP3().Phi());
+   ((TH1*) o["hRecoVsGenNeutrinoPt"]) ->Fill(mcEvent->mP4Neutrino.Pt(),  event.GetNeutrinoP3().Pt());
+   ((TH1*) o["hRecoVsGenNeutrinoPz"]) ->Fill(mcEvent->mP4Neutrino.Pz(),  event.GetNeutrinoP3().Pz());
+   ((TH1*) o["hRecoVsGenNeutrinoEta"])->Fill(mcEvent->mP4Neutrino.Eta(), event.GetNeutrinoP3().Eta());
 
-   ((TH1*) o["hRecoVsGenWBosonPhi"])  ->Fill(event.mWEvent->mP4WBoson.Phi(),   event.GetVecBosonP3().Phi());
-   ((TH1*) o["hRecoVsGenWBosonPt"])   ->Fill(event.mWEvent->mP4WBoson.Pt(),    event.GetVecBosonP3().Pt());
-   ((TH1*) o["hRecoVsGenWBosonPz"])   ->Fill(event.mWEvent->mP4WBoson.Pz(),    event.GetVecBosonP3().Pz());
-   ((TH1*) o["hRecoVsGenWBosonEta"])  ->Fill(event.mWEvent->mP4WBoson.Eta(),   event.GetVecBosonP3().Eta());
+   ((TH1*) o["hRecoVsGenWBosonPhi"])  ->Fill(mcEvent->mP4WBoson.Phi(),   event.GetVecBosonP3().Phi());
+   ((TH1*) o["hRecoVsGenWBosonPt"])   ->Fill(mcEvent->mP4WBoson.Pt(),    event.GetVecBosonP3().Pt());
+   ((TH1*) o["hRecoVsGenWBosonPz"])   ->Fill(mcEvent->mP4WBoson.Pz(),    event.GetVecBosonP3().Pz());
+   ((TH1*) o["hRecoVsGenWBosonEta"])  ->Fill(mcEvent->mP4WBoson.Eta(),   event.GetVecBosonP3().Eta());
 
-   ((TH1*) o["hGenRecoilE"]) ->Fill(event.mWEvent->mP4Recoil.E());
-   ((TH1*) o["hGenRecoilPx"])->Fill(event.mWEvent->mP4Recoil.Px());
-   ((TH1*) o["hGenRecoilPy"])->Fill(event.mWEvent->mP4Recoil.Py());
-   ((TH1*) o["hGenRecoilPz"])->Fill(event.mWEvent->mP4Recoil.Pz());
+   ((TH1*) o["hGenRecoilE"]) ->Fill(mcEvent->mP4Recoil.E());
+   ((TH1*) o["hGenRecoilPx"])->Fill(mcEvent->mP4Recoil.Px());
+   ((TH1*) o["hGenRecoilPy"])->Fill(mcEvent->mP4Recoil.Py());
+   ((TH1*) o["hGenRecoilPz"])->Fill(mcEvent->mP4Recoil.Pz());
 
-   ((TH1*) o["hRecInAccEnergy"])    ->Fill(event.mWEvent->mP4RecoilInAccept.E());
-   ((TH1*) o["hRecInAccMomentumX"]) ->Fill(event.mWEvent->mP4RecoilInAccept.Px());
-   ((TH1*) o["hRecInAccMomentumY"]) ->Fill(event.mWEvent->mP4RecoilInAccept.Py());
-   ((TH1*) o["hRecInAccMomentumZ"]) ->Fill(event.mWEvent->mP4RecoilInAccept.Pz());
-   ((TH1*) o["hRecoilInAcceptPt"])  ->Fill(event.mWEvent->mP4RecoilInAccept.Pt());
-   ((TH1*) o["hRecOutAccEnergy"])   ->Fill(event.mWEvent->mP4RecoilOutAccept.E());
-   ((TH1*) o["hRecOutAccMomentumX"])->Fill(event.mWEvent->mP4RecoilOutAccept.Px());
-   ((TH1*) o["hRecOutAccMomentumY"])->Fill(event.mWEvent->mP4RecoilOutAccept.Py());
-   ((TH1*) o["hRecOutAccMomentumZ"])->Fill(event.mWEvent->mP4RecoilOutAccept.Pz());
-   ((TH1*) o["hRecOutAccPt"])       ->Fill(event.mWEvent->mP4RecoilOutAccept.Pt());
+   ((TH1*) o["hRecInAccEnergy"])    ->Fill(mcEvent->mP4RecoilInAccept.E());
+   ((TH1*) o["hRecInAccMomentumX"]) ->Fill(mcEvent->mP4RecoilInAccept.Px());
+   ((TH1*) o["hRecInAccMomentumY"]) ->Fill(mcEvent->mP4RecoilInAccept.Py());
+   ((TH1*) o["hRecInAccMomentumZ"]) ->Fill(mcEvent->mP4RecoilInAccept.Pz());
+   ((TH1*) o["hRecoilInAcceptPt"])  ->Fill(mcEvent->mP4RecoilInAccept.Pt());
+   ((TH1*) o["hRecOutAccEnergy"])   ->Fill(mcEvent->mP4RecoilOutAccept.E());
+   ((TH1*) o["hRecOutAccMomentumX"])->Fill(mcEvent->mP4RecoilOutAccept.Px());
+   ((TH1*) o["hRecOutAccMomentumY"])->Fill(mcEvent->mP4RecoilOutAccept.Py());
+   ((TH1*) o["hRecOutAccMomentumZ"])->Fill(mcEvent->mP4RecoilOutAccept.Pz());
+   ((TH1*) o["hRecOutAccPt"])       ->Fill(mcEvent->mP4RecoilOutAccept.Pt());
 
-   ((TH2*) o["hJetRecoilPtVsWBosonPt"])                      ->Fill(event.mWEvent->mP4WBoson.Pt(),  event.GetJetRecoil().Pt());
-   ((TH2*) o["hJetRecoilPhiVsWBosonPhi"])                    ->Fill(event.mWEvent->mP4WBoson.Phi(), event.GetJetRecoil().Phi());
-   ((TH2*) o["hJetRecoilPt_GenOverReco"])                    ->Fill(event.GetJetRecoil().Pt(), event.mWEvent->mP4WBoson.Pt()/event.GetJetRecoil().Pt());
-   ((TH2*) o["hJetRecoilPt_GenOverReco_zoomin"])             ->Fill(event.GetJetRecoil().Pt(), event.mWEvent->mP4WBoson.Pt()/event.GetJetRecoil().Pt());
-   ((TH2*) o["hTrackRecoilPtVsWBosonPt"])                    ->Fill(event.mWEvent->mP4WBoson.Pt(),  event.GetTrackRecoil().Pt());
-   ((TH2*) o["hTrackRecoilPhiVsWBosonPhi"])                  ->Fill(event.mWEvent->mP4WBoson.Phi(), event.GetTrackRecoil().Phi());
-   ((TH2*) o["hTrackRecoilTpcPtVsWBosonPt"])                 ->Fill(event.mWEvent->mP4WBoson.Pt(),  event.mP3TrackRecoilTpc.Pt());
-   ((TH2*) o["hTrackRecoilTpcNeutralsPtVsWBosonPt"])         ->Fill(event.mWEvent->mP4WBoson.Pt(),  event.GetTrackRecoilTpcNeutrals().Pt());
-   ((TH2*) o["hTrackRecoilTpcNeutralsPtCorrectedVsWBosonPt"])->Fill(event.mWEvent->mP4WBoson.Pt(),  event.GetTrackRecoilTpcNeutralsCorrected().Pt());
-   ((TH2*) o["hTrackRecoilTpcNeutralsPt_GenOverReco"])       ->Fill(event.GetTrackRecoilTpcNeutrals().Pt(), event.mWEvent->mP4WBoson.Pt()/event.GetTrackRecoilTpcNeutrals().Pt());
-   ((TH2*) o["hTrackRecoilTpcNeutralsPt_GenOverReco_zoomin"])->Fill(event.GetTrackRecoilTpcNeutrals().Pt(), event.mWEvent->mP4WBoson.Pt()/event.GetTrackRecoilTpcNeutrals().Pt());
+   ((TH2*) o["hJetRecoilPtVsWBosonPt"])                      ->Fill(mcEvent->mP4WBoson.Pt(),  event.GetJetRecoil().Pt());
+   ((TH2*) o["hJetRecoilPhiVsWBosonPhi"])                    ->Fill(mcEvent->mP4WBoson.Phi(), event.GetJetRecoil().Phi());
+   ((TH2*) o["hJetRecoilPt_GenOverReco"])                    ->Fill(event.GetJetRecoil().Pt(), mcEvent->mP4WBoson.Pt()/event.GetJetRecoil().Pt());
+   ((TH2*) o["hJetRecoilPt_GenOverReco_zoomin"])             ->Fill(event.GetJetRecoil().Pt(), mcEvent->mP4WBoson.Pt()/event.GetJetRecoil().Pt());
+   ((TH2*) o["hTrackRecoilPtVsWBosonPt"])                    ->Fill(mcEvent->mP4WBoson.Pt(),  event.GetTrackRecoil().Pt());
+   ((TH2*) o["hTrackRecoilPhiVsWBosonPhi"])                  ->Fill(mcEvent->mP4WBoson.Phi(), event.GetTrackRecoil().Phi());
+   ((TH2*) o["hTrackRecoilTpcPtVsWBosonPt"])                 ->Fill(mcEvent->mP4WBoson.Pt(),  event.mP3TrackRecoilTpc.Pt());
+   ((TH2*) o["hTrackRecoilTpcNeutralsPtVsWBosonPt"])         ->Fill(mcEvent->mP4WBoson.Pt(),  event.GetTrackRecoilTpcNeutrals().Pt());
+   ((TH2*) o["hTrackRecoilTpcNeutralsPtCorrectedVsWBosonPt"])->Fill(mcEvent->mP4WBoson.Pt(),  event.GetTrackRecoilTpcNeutralsCorrected().Pt());
+   ((TH2*) o["hTrackRecoilTpcNeutralsPt_GenOverReco"])       ->Fill(event.GetTrackRecoilTpcNeutrals().Pt(), mcEvent->mP4WBoson.Pt()/event.GetTrackRecoilTpcNeutrals().Pt());
+   ((TH2*) o["hTrackRecoilTpcNeutralsPt_GenOverReco_zoomin"])->Fill(event.GetTrackRecoilTpcNeutrals().Pt(), mcEvent->mP4WBoson.Pt()/event.GetTrackRecoilTpcNeutrals().Pt());
 
-   Double_t recoilRelDiff = (event.GetJetRecoil().Pt() - event.mWEvent->mP4WBoson.Pt())/event.mWEvent->mP4WBoson.Pt();
+   Double_t recoilRelDiff = (event.GetJetRecoil().Pt() - mcEvent->mP4WBoson.Pt())/mcEvent->mP4WBoson.Pt();
    ((TH1*) o["hJetRecoilPtRelDiff"]) ->Fill( recoilRelDiff );
 
-   recoilRelDiff = (event.GetTrackRecoil().Pt() - event.mWEvent->mP4WBoson.Pt())/event.mWEvent->mP4WBoson.Pt();
+   recoilRelDiff = (event.GetTrackRecoil().Pt() - mcEvent->mP4WBoson.Pt())/mcEvent->mP4WBoson.Pt();
    ((TH1*) o["hTrackRecoilPtRelDiff"]) ->Fill( recoilRelDiff );
 
-   recoilRelDiff = (event.GetTrackRecoilTpcNeutrals().Pt() - event.mWEvent->mP4WBoson.Pt())/event.mWEvent->mP4WBoson.Pt();
+   recoilRelDiff = (event.GetTrackRecoilTpcNeutrals().Pt() - mcEvent->mP4WBoson.Pt())/mcEvent->mP4WBoson.Pt();
    ((TH1*) o["hTrackRecoilTpcNeutralsPtRelDiff"]) ->Fill( recoilRelDiff );
 
    recoilRelDiff = (event.GetTrackRecoil().Pt() - event.GetJetRecoil().Pt())/event.GetJetRecoil().Pt();
