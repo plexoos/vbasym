f36132a5a7e2b3dc46fd60f629b37bd24cbf3176
Dmitri Smirnov <d.s@plexoos.com>
2013-11-13 07:23:50 -0500

    Beautified

--- src/events_calc.cc
+++ src/events_calc.cc
@@ -2,55 +2,37 @@
 #include <cstdlib>
 #include <cassert>
 #include <iostream>
-#include <fstream>  
-
-//#include "TObject.h"
-//#include "TIterator.h"
-//#include "TROOT.h"
-//#include "TString.h"
-
-#include "utils/utils.h"
+#include <fstream>
 
 using namespace std;
 
 
 int main(int argc, char *argv[])
 {
+   float evtCount;
+   float eventsSum = 0;
+   string stringfname;
 
-float evtCount;
-float eventsSum;
-string stringfname;
+   //string filelist = "./runlists/run11_transverse_embed_gt100evt_test";
+   //cout << "Opening file: " << filelist << endl;
 
-//string filelist       = "./runlists/run11_transverse_embed_gt100evt_test";
+   ifstream infile;
+   //infile.open ("./runlists/run11_transverse_embed_gt500evt_evtcount2");
+   infile.open ("~vbasym/runlists/run11_transverse_embed_gt500evt_evtcount");
 
+   if(!infile)
+   {
+       cout << "Can't open file" << endl;
+       exit(EXIT_FAILURE);
+   }
 
- eventsSum=0.;
+   // while (!infile.eof())
+   while (infile >> stringfname >> evtCount) // second coulmn overrides the first
+   {
+      //infile >> evtCount;
+      //infile.close();
+      eventsSum += evtCount;
+   }
 
- 
- //     cout << "Opening file: " << filelist << endl;
-
-
- ifstream infile;
- //infile.open ("./runlists/run11_transverse_embed_gt500evt_evtcount2");
- infile.open ("~vbasym/runlists/run11_transverse_embed_gt500evt_evtcount");
-
-if(!infile)
-{
-    cout << "Can't open file";
-    exit(EXIT_FAILURE);
-}
-
-// while (!infile.eof())
- while (infile >> stringfname >> evtCount) // second coulmn overrides the first
- { 
-   //    infile >> evtCount;
-   //    infile.close();
-
-
-    eventsSum += evtCount;
-  
- }
-
- cout << "Number of events: " << eventsSum << endl;;
-
+   cout << "Number of events: " << eventsSum << endl;
 }
