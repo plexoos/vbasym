9470e918ebd069dc8c74556b0e88907bce250449
Dmitri Smirnov <d.s@plexoos.com>
2013-12-17 17:51:00 -0500

    Style issues addressed

--- src/lumi_calc.cc
+++ src/lumi_calc.cc
@@ -1,12 +1,10 @@
 #include <string>
 #include <cstdlib>
-#include <cassert>
 #include <iostream>
-#include <fstream>  
+#include <fstream>
 
 #include "TObject.h"
 #include "TIterator.h"
-#include "TROOT.h"
 #include "TString.h"
 
 #include "utils/utils.h"
@@ -16,50 +14,47 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+   float lumiEff;
+   float lumiTot;
+   float lumiEffSum = 0;
+   float lumiTotSum = 0;
 
-float lumiEff;
-float lumiTot;
-float lumiEffSum;
-float lumiTotSum;
+   string filelist = "./runlists/run11_pp_transverse";
 
-string filelist       = "./runlists/run11_pp_transverse";
+   TObject *o;
+   TIter   *next = new TIter(utils::getFileList(filelist));
 
-TObject *o;
-TIter   *next = new TIter(utils::getFileList(filelist));
+   while (next && (o = (*next)()) )
+   {
+       string runNo    = string(((TObjString*) o)->GetName());
 
- lumiEffSum=0.;
- lumiTotSum=0.;
+       TString pathLumi =  "/star/data05/scratch/fazio/lumi_run11_cut05/";
+       TString lumiEffName = pathLumi;
+       lumiEffName += "R" + runNo + "_lumi_effective.txt";
+       TString lumiTotalName = pathLumi;
+       lumiTotalName += "R" + runNo + "_lumi_total.txt";
 
- while (next && (o = (*next)()) )
- {
-     string runNo    = string(((TObjString*) o)->GetName());
+       cout << "Opening file: " << lumiEffName << endl;
 
-     TString pathLumi =  "/star/data05/scratch/fazio/lumi_run11_cut05/";
-     TString lumiEffName = pathLumi;  
-     lumiEffName += "R" + runNo + "_lumi_effective.txt";
-     TString lumiTotalName = pathLumi;  
-     lumiTotalName += "R" + runNo + "_lumi_total.txt";
+       ifstream lumie;
+       lumie.open (lumiEffName);
+       lumie >> lumiEff;
+       lumie.close();
 
-     cout << "Opening file: " << lumiEffName << endl;
+       cout << "Opening file: " << lumiTotalName << endl;
 
-     ifstream lumie;
-     lumie.open (lumiEffName);
-     lumie >> lumiEff;
-     lumie.close();
+       ifstream lumit;
+       lumit.open (lumiTotalName);
+       lumit >> lumiTot;
+       lumit.close();
 
-     cout << "Opening file: " << lumiTotalName << endl;
+       lumiEffSum += lumiEff;
+       lumiTotSum += lumiTot;
 
-     ifstream lumit;
-     lumit.open (lumiTotalName);
-     lumit >> lumiTot;
-     lumit.close();
-     
-     lumiEffSum += lumiEff;
-     lumiTotSum += lumiTot;
+   }
 
- }
+   cout << "Effective luminosity (pb-1): " << lumiEffSum << endl;
+   cout << "Total luminosity (pb-1): " << lumiTotSum << endl;
 
- cout << "Effective luminosity (pb-1): " << lumiEffSum << endl;
- cout << "Total luminosity (pb-1): " << lumiTotSum << endl;
-
+   return EXIT_SUCCESS;
 }
