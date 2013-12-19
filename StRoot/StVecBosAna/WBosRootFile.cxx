9af9cd9768cbde55d5a9e2224951484625a4bbcb
sfazio <sfazio@bnl.gov>
2013-12-19 18:05:38 -0500

    AllAsymHContainer filled for data only

--- StRoot/StVecBosAna/WBosRootFile.cxx
+++ StRoot/StVecBosAna/WBosRootFile.cxx
@@ -71,10 +71,10 @@ void WBosRootFile::BookHists()
    fHists->d["event_wm"] = ph = new WBosEventHContainer(new TDirectoryFile("event_wm", "event_wm", "", this));
    fHistCuts[kCUT_EVENT_W_MINUS].insert(ph);
 
+   if (!fIsMc) return;
+
    fHists->d["asym"] = ph = new AllAsymHContainer(new TDirectoryFile("asym", "asym", "", this));
    fHistCuts[kCUT_EVENT_W].insert(ph);
-
-   if (!fIsMc) return;
 
    fHists->d["event_mc"] = ph = new MCHContainer(new TDirectoryFile("event_mc", "event_mc", "", this));
    fHistCuts[kCUT_EVENT_W].insert(ph);
