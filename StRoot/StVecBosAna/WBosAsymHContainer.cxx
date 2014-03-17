3cb27695f64ca6c86d392190e5ef152beb9a982b
sfazio <sfazio@bnl.gov>
2014-03-17 16:52:00 -0400

    changed binning for asymmetries versus W-Pt.

--- StRoot/StVecBosAna/WBosAsymHContainer.cxx
+++ StRoot/StVecBosAna/WBosAsymHContainer.cxx
@@ -46,6 +46,8 @@ void WBosAsymHContainer::BookHists()
    rh::MultiGraph *mg;
 
    DoubleSpinStateSetIter iDSS = gDoubleSpinStateSet.begin();
+   Double_t xBinsPt[8] = {0, 1, 2, 3, 4, 5, 6, 10};
+
    for ( ; iDSS!=gDoubleSpinStateSet.end(); ++iDSS)
    {
       EDoubleSpinState dss = *iDSS;
@@ -60,7 +62,8 @@ void WBosAsymHContainer::BookHists()
       o[shName] = fYieldPhiVsRap_Dss[dss] = h2i;
 
       shName = "hWBosonPhiVsPt_" + sDblSpinState;
-      h2i = new rh::H2I(shName.c_str(), "; W Boson P_{T}; W Boson #phi;", 10, 0, 10, 8, -M_PI, M_PI, "colz DUMP");
+      //h2i = new rh::H2I(shName.c_str(), "; W Boson P_{T}; W Boson #phi;", 10, 0, 10, 8, -M_PI, M_PI, "colz DUMP");
+      h2i = new rh::H2I(shName.c_str(), "; W Boson P_{T}; W Boson #phi;", 7, xBinsPt, 8, -M_PI, M_PI, "colz DUMP");
       o[shName] = fYieldPhiVsPt_Dss[dss] = h2i;
    }
 
@@ -79,7 +82,8 @@ void WBosAsymHContainer::BookHists()
       o[shName] = fYieldPhiVsRap_Sss[sss] = h2i;
 
       shName = "hWBosonPhiVsPt_" + sSnglSpinState;
-      h2i = new rh::H2I(shName.c_str(), "; W Boson P_{T}; W Boson #phi;", 10, 0, 10, 8, -M_PI, M_PI, "colz DUMP");
+      //h2i = new rh::H2I(shName.c_str(), "; W Boson P_{T}; W Boson #phi;", 10, 0, 10, 8, -M_PI, M_PI, "colz DUMP");
+      h2i = new rh::H2I(shName.c_str(), "; W Boson P_{T}; W Boson #phi;", 7, xBinsPt, 8, -M_PI, M_PI, "colz DUMP");
       o[shName] = fYieldPhiVsPt_Sss[sss] = h2i;
 
       shName = "hWBosonPhi_PtProj_" + sSnglSpinState;
@@ -101,14 +105,14 @@ void WBosAsymHContainer::BookHists()
       // Asymmetry amplitude vs eta
       shName = "hWBosonAsymAmpVsEta_" + sBeam;
       h1d = new rh::H1D(shName.c_str(), "; W Boson #eta; Asym Amp.;", 6, -6, 6, "E1 GRIDX GRIDY");
-      h1d->GetYaxis()->SetRangeUser(-0.5, 0.5);
+      h1d->GetYaxis()->SetRangeUser(-1.5, 1.5);
       o[shName] = fAsymAmpVsEta_Beam[beamId] = h1d;
 
       // Multigraph container with graphs for individual slices/bins
       shName = "mgrWBosonAsymVsPhi_EtaBins_" + sBeam;
       mg = new rh::MultiGraph(shName, shName);
       h1f = new TH1F(shName.c_str(), "; W Boson #phi; Asym.;", 1, -M_PI, M_PI);
-      h1f->GetYaxis()->SetRangeUser(-0.5, 0.5);
+      h1f->GetYaxis()->SetRangeUser(-1.5, 1.5);
       mg->SetHistogram(h1f);
       o[shName] = fAsymVsPhi_EtaBins_Beam[beamId] = mg;
 
@@ -120,50 +124,53 @@ void WBosAsymHContainer::BookHists()
       // Asymmetry amplitude vs rapidity
       shName = "hWBosonAsymAmpVsRap_" + sBeam;
       h1d = new rh::H1D(shName.c_str(), "; W Boson Rapidity; Asym Amp.;", 6, -1.5, 1.5, "E1 GRIDX GRIDY");
-      h1d->GetYaxis()->SetRangeUser(-0.5, 0.5);
+      h1d->GetYaxis()->SetRangeUser(-1.5, 1.5);
       o[shName] = fAsymAmpVsRap_Beam[beamId] = h1d;
 
       // Multigraph container with graphs for individual slices/bins
       shName = "mgrWBosonAsymVsPhi_RapBins_" + sBeam;
       mg = new rh::MultiGraph(shName, shName);
       h1f = new TH1F(shName.c_str(), "; W Boson #phi; Asym.;", 1, -M_PI, M_PI);
-      h1f->GetYaxis()->SetRangeUser(-0.5, 0.5);
+      h1f->GetYaxis()->SetRangeUser(-1.5, 1.5);
       mg->SetHistogram(h1f);
       o[shName] = fAsymVsPhi_RapBins_Beam[beamId] = mg;
 
       // Asymmetry vs phi vs p_T
       shName = "hWBosonAsymVsPhiVsPt_" + sBeam;
-      h2d = new rh::H2D(shName.c_str(), "; W Boson P_{T}; W Boson #phi;", 10, 0, 10, 8, -M_PI, M_PI, "colz");
+      //h2d = new rh::H2D(shName.c_str(), "; W Boson P_{T}; W Boson #phi;", 10, 0, 10, 8, -M_PI, M_PI, "colz");
+      h2d = new rh::H2D(shName.c_str(), "; W Boson P_{T}; W Boson #phi;", 7, xBinsPt, 8, -M_PI, M_PI, "colz");
       o[shName] = fAsymVsPhiVsPt_Beam[beamId] = h2d;
 
       // Asymmetry amplitude vs p_T
       shName = "hWBosonAsymAmpVsPt_" + sBeam;
-      h1d = new rh::H1D(shName.c_str(), "; W Boson P_{T}; Asym Amp.;", 10, 0, 10, "E1 GRIDX GRIDY");
-      h1d->GetYaxis()->SetRangeUser(-0.5, 0.5);
+      //h1d = new rh::H1D(shName.c_str(), "; W Boson P_{T}; Asym Amp.;", 10, 0, 10, "E1 GRIDX GRIDY");
+      h1d = new rh::H1D(shName.c_str(), "; W Boson P_{T}; Asym Amp.;", 7, xBinsPt, "E1 GRIDX GRIDY");
+      h1d->GetYaxis()->SetRangeUser(-1.5, 1.5);
       o[shName] = fAsymAmpVsPt_Beam[beamId] = h1d;
 
       // Multigraph container with graphs for individual slices/bins
       shName = "mgrWBosonAsymVsPhi_PtBins_" + sBeam;
       mg = new rh::MultiGraph(shName, shName);
       h1f = new TH1F(shName.c_str(), "; W Boson #phi; Asym.;", 1, -M_PI, M_PI);
-      h1f->GetYaxis()->SetRangeUser(-0.5, 0.5);
+      h1f->GetYaxis()->SetRangeUser(-1.5, 1.5);
       mg->SetHistogram(h1f);
       o[shName] = fAsymVsPhi_PtBins_Beam[beamId] = mg;
    }
 
    shName = "hWBosonAsymAmpVsEta_";
    h1d = new rh::H1D(shName.c_str(), "; W Boson #eta; Asym Amp.;", 6, -6, 6, "E1 GRIDX GRIDY");
-   h1d->GetYaxis()->SetRangeUser(-0.5, 0.5);
+   h1d->GetYaxis()->SetRangeUser(-1.5, 1.5);
    o[shName] = fAsymAmpVsEta = h1d;
 
    shName = "hWBosonAsymAmpVsRap_";
    h1d = new rh::H1D(shName.c_str(), "; W Boson Rapidity; Asym Amp.;", 6, -1.5, 1.5, "E1 GRIDX GRIDY");
-   h1d->GetYaxis()->SetRangeUser(-0.5, 0.5);
+   h1d->GetYaxis()->SetRangeUser(-1.5, 1.5);
    o[shName] = fAsymAmpVsRap = h1d;
 
    shName = "hWBosonAsymAmpVsPt_";
-   h1d = new rh::H1D(shName.c_str(), "; W Boson P_{T}; Asym Amp.;", 10, 0, 10, "E1 GRIDX GRIDY");
-   h1d->GetYaxis()->SetRangeUser(-0.5, 0.5);
+   //h1d = new rh::H1D(shName.c_str(), "; W Boson P_{T}; Asym Amp.;", 10, 0, 10, "E1 GRIDX GRIDY");
+   h1d = new rh::H1D(shName.c_str(), "; W Boson P_{T}; Asym Amp.;", 7, xBinsPt, "E1 GRIDX GRIDY");
+   h1d->GetYaxis()->SetRangeUser(-1.5, 1.5);
    o[shName] = fAsymAmpVsPt = h1d;
 }
 
